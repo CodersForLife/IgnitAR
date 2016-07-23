39036cc6ca15c4bfd23c6c5b2eb25001da9e7a06
Nimit Agg <nimitagg95@gmail.com>
2016-07-24 01:58:17 +0530

    splash screen added

---
+++ app/src/main/java/com/vizy/ignitar/activities/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.vizy.ignitar.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vizy.ignitar.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
