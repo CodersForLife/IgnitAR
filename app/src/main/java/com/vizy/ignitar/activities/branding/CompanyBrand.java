30717102609f37b3920919032f860de4239f6ec2
AadityaDev <adityaverma.dev@gmail.com>
2017-02-13 21:35:56 +0530

    company branding page added

--- app/src/main/java/com/vizy/ignitar/activities/branding/CompanyBrand.java
+++ app/src/main/java/com/vizy/ignitar/activities/branding/CompanyBrand.java
@@ -1,5 +1,6 @@
 package com.vizy.ignitar.activities.branding;
 
+import android.animation.Animator;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.vizy.ignitar.R;
 import com.vizy.ignitar.ui.customviews.CameraView;
@@ -18,6 +20,14 @@ public class CompanyBrand extends AppCompatActivity {
     private Camera camera;
     private CameraView cameraView;
 
+    private ImageView kfcCal;
+    private ImageView kfcFB;
+    private ImageView kfcTWITTER;
+    private ImageView kfcLogo;
+    private ImageView kfcOfferButton;
+    private ImageView kfcReviewUs;
+    private ImageView kfcSelfie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +38,45 @@ public class CompanyBrand extends AppCompatActivity {
             actionBar.hide();
         }
 
+        kfcCal = (ImageView) findViewById(R.id.kfc_calendar);
+        kfcFB = (ImageView) findViewById(R.id.kfc_facebook);
+        kfcTWITTER = (ImageView) findViewById(R.id.kfc_twitter);
+        kfcOfferButton = (ImageView) findViewById(R.id.kfc_offer);
+        kfcReviewUs = (ImageView) findViewById(R.id.kfc_review);
+        kfcSelfie = (ImageView) findViewById(R.id.kfc_selfie);
+
+
         camera = getCameraInstance();
         if (camera != null) {
             cameraView = new CameraView(this, camera);
             FrameLayout preview = (FrameLayout) findViewById(R.id.camera_view);
             preview.addView(cameraView);
         }
+
+
+        kfcCal.animate().rotationYBy(45f).rotationXBy(180f).setDuration(9000).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        kfcTWITTER.animate().translationX(-150f).setDuration(9000);
+        kfcFB.animate().translationX(150f).setDuration(9000);
     }
 
     @Override
